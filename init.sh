6c7fcc8fa97035dc958d70278c13736942f41856
Kevin Du <db_00@msn.com>
2018-03-09 09:46:05 +0800

    config added

---
+++ init.sh
@@ -0,0 +1,15 @@
+clear
+echo ""
+echo "#############################################################"
+echo "# Automatically install my environment and init config      #"
+echo "# Intro: https://github.com/felmoon/mecy                    #"
+echo "# Author: felmoon<felmoon@qq.com>                           #"
+echo "#############################################################"
+echo 
+
+
+# Install JAVA
+
+# Install Git
+
+# Copy Configuration
